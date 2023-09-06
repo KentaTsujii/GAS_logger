e2dcf0d8716a3d1b0802a1e5dee426ae2a07c4bc
Kenta Tsujii <k.tsujii@datumstudio.jp>
2023-09-06 12:55:02 +0900

    fix: テストの間違い修正

--- logger_test.ts
+++ logger_test.ts
@@ -1,7 +1,7 @@
 function custom_logger_test(){
   const logger = GasLogger.get_logger();
-  logger.add_logger(GasLogger.create_appender("FileLogger", "logging_test", "test"))
-  logger.add_logger(GasLogger.create_appender("ConsoleLogger", "logging_test", "test"))
+  logger.add_logger(GasLogger.create_appender("FileAppender", "logging_test", "test"))
+  logger.add_logger(GasLogger.create_appender("ConsoleAppender", "logging_test", "test"))
 
   logger.write_log("new log");
   logger.write_log("new_log2");
